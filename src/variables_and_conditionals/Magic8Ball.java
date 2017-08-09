b4a0ceb9b03f56a241ec42cd0f927ab464965e6f
samus114 <league@WTS22.attlocal.net>
2017-08-09 11:56:35 -0700

    slrkgtm

--- src/variables_and_conditionals/Magic8Ball.java
+++ src/variables_and_conditionals/Magic8Ball.java
@@ -2,30 +2,37 @@ package variables_and_conditionals;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Magic8Ball {
 
     // 1. Make a main method that includes all the steps below….
-
+public static void main(String[] args) {
     // 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	Random rand=new Random();
+	int r= rand.nextInt(4);
     // 3. Print out this variable
-
+System.out.println(r);
     // 4. Get the user to enter a question for the 8 ball
-
+String lol=JOptionPane.showInputDialog("insert a yes or no question");
     // 5. If the random number is 0
-
+if(r==0){
+JOptionPane.showMessageDialog(null, "Yes");}
     // -- tell the user "Yes"
 
     // 6. If the random number is 1
-
+if(r==1){
+JOptionPane.showMessageDialog(null, "No");}
     // -- tell the user "No"
 
     // 7. If the random number is 2
-
+if(r==2){
+JOptionPane.showMessageDialog(null, "Maybe you should ask google?");}
     // -- tell the user "Maybe you should ask Google?"
 
     // 8. If the random number is 3
-
+if(r==3){
+JOptionPane.showMessageDialog(null, "ask again later");}
     // -- write your own answer
 
-}
+}}
--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,29 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
-        // 2. Ask the user to enter a name. Store their answer in a variable.
-
+String samson="super gamer";
+String ollie="invisiblity";       
+String june="writing recipes";		
+String chris="super speed";
+String micheal="super quiet";
+String sofia="inability to deside";
+String andrew="to speak";	
+		// 2. Ask the user to enter a name. Store their answer in a variable.
+String lol=JOptionPane.showInputDialog("type in a name plz");
         // 3. Show the superpower in a pop-up, depending on the name entered. 
-
-    }
+if(lol.equals("samson")){
+JOptionPane.showMessageDialog(null, "samson's superpower is "+samson);}
+if(lol.equals("ollie")){
+JOptionPane.showMessageDialog(null, "ollie's superpower is "+ollie);}
+if(lol.equals("june")){
+JOptionPane.showMessageDialog(null, "june's super power is "+june);}
+if(lol.equals("chris")){
+JOptionPane.showMessageDialog(null, "chris's super power is "+chris);}
+if(lol.equals("micheal")){
+JOptionPane.showMessageDialog(null, "micheals superpower is "+micheal);}
+if(lol.equals("sofia")){
+JOptionPane.showMessageDialog(null, "sofia's super power is "+sofia);}
+if(lol.equals("andrew")){
+JOptionPane.showMessageDialog(null,"andrew's superpower is "+andrew);}
+	}
 }
