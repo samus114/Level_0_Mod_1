b94090ca6dbbeb0ce51aab637370cd5b920233cb
samus114 <league@WTS22.attlocal.net>
2017-08-09 11:58:33 -0700

    drth

--- src/variables_and_conditionals/ChangeCalculator.java
+++ src/variables_and_conditionals/ChangeCalculator.java
@@ -17,7 +17,7 @@ public class ChangeCalculator {
     public static void main(String[] args) {
 
         // Ask the user how many nickels they have
-
+Object pie=JOptionPane.showInputDialog("how many nickels do you have");
         // Convert their answer to an int using Integer.parseInt()
 
         // Ask the user how many dimes they have, and convert their answer
